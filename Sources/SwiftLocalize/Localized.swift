f62b8d9f1adaa74ede039eaebd11efebefa4e9c8
Danil <voidilov@gmail.com>
2026-05-20 18:27:24 +0300

    Refactor language resolution in Localized concatenation to prioritize user preferred languages

--- Sources/SwiftLocalize/Localized.swift
+++ Sources/SwiftLocalize/Localized.swift
@@ -150,11 +150,10 @@ extension Localized where Value: RangeReplaceableCollection {
 			baseValue = lhs.base.value + rhs.resolved(lhs.base.language!)
 		} else {
 			var translationsKeys = Set(mergedTranslations.keys)
+			let priorityLanguages = [lhs.base.language!, rhs.base.language!] + Locale.preferredLanguages.map(Language.init(rawValue:))
 			let language: Language?
-			if translationsKeys.contains(lhs.base.language!) {
-				language = lhs.base.language
-			} else if translationsKeys.contains(rhs.base.language!) {
-				language = rhs.base.language
+			if let lang = priorityLanguages.first(where: translationsKeys.contains) {
+				language = lang
 			} else {
 				language = translationsKeys.sorted(by: { $0.rawValue < $1.rawValue }).first
 			}
